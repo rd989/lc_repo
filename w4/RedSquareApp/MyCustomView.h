321488a0ad62a06094512b1606680623a19c9659
Rodrigo <rd989@nyu.edu>
2009-02-22 16:48:19 -0500

    RedSquare first commit

---
+++ w4/RedSquareApp/MyCustomView.h
@@ -0,0 +1,24 @@
+//
+//  MyCustomView.h
+//  RedSquareApp
+//
+//  Created by Rodrigo on 2/20/09.
+//  Copyright 2009 New York University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MyCustomView : UIView {
+	CGFloat squareSize;
+	CGFloat rotation;
+	CGColorRef aColor;
+	BOOL twoFingers;
+	
+	IBOutlet UILabel *xField;
+	IBOutlet UILabel *yField;
+	IBOutlet UILabel *zField;
+	
+}
+
+@end
