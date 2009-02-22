2f215bbde37aee03574d804e930db4c46815d0fd
Rodrigo <rd989@nyu.edu>
2009-02-22 16:10:15 -0500

    Added struct definitions

--- w3/structgit.c
+++ w3/structgit.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
+// Define Vector
+typedef struct Vector
+{
+  float x;
+  float y;
+} Vector;
+ 
+typedef Vector* VectorRef;
+ 
+// Define LinkedVector
+typedef struct LinkedVector
+{
+  Vector point;
+  struct LinkedVector* next;
+} LinkedVector;
+ 
+typedef LinkedVector* LinkedVectorRef;
+
+
 int main(int argc, char** argv)
 {
   printf("Hello, world!\n");
   printf("A new line!\n"); 
+printf("Added on the develop branch!\n");
   return 0;
 }
